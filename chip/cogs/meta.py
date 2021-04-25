b85a132473aa9cf6503f601ba1e8059abf2fd38e
EEKIM10 <eek@clicksminuteper.net>
2021-04-25 14:25:05 +0100

    Add the info command

--- chip/cogs/meta.py
+++ chip/cogs/meta.py
@@ -1,7 +1,13 @@
 import time
+import aiohttp
 from datetime import datetime
+from subprocess import run as run_shell
+from functools import partial
 
+import aiosqlite
 import discord
+import jishaku
+import tabulate
 from discord.ext import commands
 
 from ..bot import ChipBot
@@ -11,6 +17,15 @@ class Meta(commands.Cog):
     """This cog contains commands for meta about the bot (e.g. version, credits, ping)."""
     def __init__(self, bot: ChipBot):
         self.bot = bot
+
+    async def run_async(self, part):
+        """
+        Async runs a blocking function in the event executor (makes it non-blocking)
+
+        :param part: a functools.partial function.
+        :return: The result of the partial
+        """
+        return await self.bot.loop.run_in_executor(None, part)
 
     @commands.command(name="ping", aliases=['pong'])
     @commands.bot_has_permissions(embed_links=True)
@@ -40,6 +55,48 @@ class Meta(commands.Cog):
     async def credits(self, ctx: commands.Context):
         """Displays loads of metadata about the bot."""
         msg = await ctx.send("Loading...")
+        head = await self.run_async(partial(run_shell, ["git", "rev-parse", "HEAD", "--short"], shell=True))
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://api.github.com/repos/dragdev-studios/chip/tags") as response:
+                data = await response.json()
+                if response.status != 200 or not data:
+                    out_of_date = False
+                    our_version = "0.0.0a"
+                    their_version = "999.999.999-post"
+                    newest_release = {
+                        "name": "haha it broke"
+                    }
+                else:
+                    our_version = tuple(self.bot.__version__.split("."))
+                    newest_release = data[0]
+                    newest_version = tuple(newest_release["name"].split("."))
+                    out_of_date = newest_version > our_version
+
+        embed = discord.Embed(
+            title="About Chip:",
+            description="ChipBot is open-source! Contribute [here](https://github.com/dragdev-studios/chip)",
+            colour=discord.Colour.orange(),
+            timestamp=datetime.utcnow()
+        )
+
+        if out_of_date:
+            newest_release_text = f"Newest Release: [v{newest_release['name']}](https://github.com/dragdev-studios/" \
+                                  f"chip/releases/{newest_release['name']})"
+        else:
+            newest_release_text = ""
+        embed.add_field(
+            name="Bot Version:",
+            value=f"{self.bot.__version__}\nRelease: {our_version}\n{newest_release_text}"
+        )
+        embed.add_field(
+            name="Dependency Versions:",
+            value=f"discord.py: {discord.__version__}\n"
+                  f"Jishaku: {jishaku.__version__}\n"
+                  f"Tabulate: {tabulate.__version__}\n"
+                  f"aiosqlite: {aiosqlite.__version__}",
+            inline=False
+        )
+        return await msg.edit(content=None, embed=embed)
 
 
 def setup(bot):
